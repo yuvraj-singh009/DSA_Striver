98d5aef263cc97dfe3cefb7f54b65302f14d03fb
Yuvraj Singh <mailatyuvrajsingh@gmail.com>
2026-07-06 23:08:15 +0530

    first code

---
+++ Learn_The_Basics/input.java
@@ -0,0 +1,12 @@
+package Learn_The_Basics;
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        sc.close();
+    }
+}
