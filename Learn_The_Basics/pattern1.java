959e14bc0035abb63fe3ab1b68c0105c81aea6c0
Yuvraj Singh <mailatyuvrajsingh@gmail.com>
2026-07-09 09:20:14 +0530

    patterns

---
+++ Learn_The_Basics/pattern1.java
@@ -0,0 +1,18 @@
+package Learn_The_Basics;
+import java.util.*;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
