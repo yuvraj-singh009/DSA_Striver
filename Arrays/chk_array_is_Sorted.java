5045d03b27d9ef826fcbddfd5fdf12471e0a1a12
Yuvraj Singh <mailatyuvrajsingh@gmail.com>
2026-07-12 21:46:24 +0530

    Add algorithm to check if an array is sorted in ascending order

---
+++ Arrays/chk_array_is_Sorted.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class chk_array_is_Sorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        boolean isSorted = true;
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                isSorted = false;
+                break;
+            }
+        }
+        if(isSorted){
+            System.out.println("The array is sorted in ascending order.");
+        } else {
+            System.out.println("The array is not sorted in ascending order.");
+        }
+    }
+}
