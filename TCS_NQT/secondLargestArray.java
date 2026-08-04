5a5b592ddcd8407da9c384ecf1cb0ff639f6627c
Yuvraj Singh <mailatyuvrajsingh@gmail.com>
2026-08-04 16:22:42 +0530

    Add implementation of second largest element finder in an array

---
+++ TCS_NQT/secondLargestArray.java
@@ -0,0 +1,24 @@
+package TCS_NQT;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class secondLargestArray {
+     public static void main(String args[]){
+		
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for(int i=n-1;i>=0;i--){
+            if(arr[i]!=arr[n-1]){
+                System.out.print(arr[i]);
+                break;
+            }
+			
+		}
+	}
+}
