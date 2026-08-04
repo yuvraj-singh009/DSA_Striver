e55e4ab71f1311ae571f7917401a574df829dd20
Yuvraj Singh <mailatyuvrajsingh@gmail.com>
2026-08-04 14:15:17 +0530

    Add implementation of second smallest element finder in an array

---
+++ TCS_NQT/secondSmallestArray.java
@@ -0,0 +1,24 @@
+package TCS_NQT;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class secondSmallestArray {
+     public static void main(String args[]){
+		
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for(int i=0;i<n;i++){
+            if(arr[i]!=arr[0]){
+                System.out.print(arr[i]);
+                break;
+            }
+			
+		}
+	}
+}
