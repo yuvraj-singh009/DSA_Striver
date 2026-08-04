15fc082534525a636e40e5c6f0be8a3c50dd9abb
Yuvraj Singh <mailatyuvrajsingh@gmail.com>
2026-08-04 14:08:50 +0530

    Add implementation of smallest element finder in an array

---
+++ TCS_NQT/smallestArray.java
@@ -0,0 +1,18 @@
+package TCS_NQT;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class smallestArray {
+     public static void main(String args[]){
+		
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		System.out.print(arr[0]);
+	}
+}
