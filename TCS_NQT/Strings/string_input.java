ad7dfe09136154ba74221820cc5d4e17e9367805
Yuvraj Singh <mailatyuvrajsingh@gmail.com>
2026-08-11 21:18:50 +0530

    Add initial implementation for string input and output

---
+++ TCS_NQT/Strings/string_input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class string_input {
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	String st = sc.nextLine();
+	System.out.println(st);
+	}
+}
